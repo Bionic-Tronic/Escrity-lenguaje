7d0e9a7ad290c59ad8d872e5a75dee17f73be17b
Bionic-Tronic <quevainacongithub@gmail.com>
2024-12-22 19:28:26 -0500

    Update

--- src/main.c
+++ src/main.c
@@ -2,19 +2,23 @@
 #include "../include/token.h"
 #include "../include/variable.h"
 #include "../include/function.h"
+#include "../include/object.h"
+#include "../include/enum.h"
+#include "../include/Array.h"
 #include "../include/evaluate.h"
 #include "../include/interpret.h"
 
-int main() {
-    FILE * fp = fopen("main.ty","r");
-    if(fp == NULL){
-    	perror("");
-    	return -1;
-    }
-    char code[1024];
-    fread(code, sizeof(char), 1024, fp);
-    fclose(fp);
-    tokenize(code);
-    interpret();
-    return 0;
+int main(){
+	FILE *fp = fopen("main.ty", "r");
+	if (fp == NULL){
+		perror("");
+		return -1;
+	}
+	char code[1024];
+	fread(code, sizeof(char), 1024, fp);
+	fclose(fp);
+	system("color 8");
+	tokenize(code);
+	interpret();
+	return 0;
 }
